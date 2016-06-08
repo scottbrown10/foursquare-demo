7752840a0a1d86e6fd8eb6707cfa3dc26015d382
Scott <scott@qburst.com>
2016-06-08 08:36:08 -0400

    Updated layout with title and list view

--- app/src/main/java/com/example/scott/foursquare/MainActivity.java
+++ app/src/main/java/com/example/scott/foursquare/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.scott.foursquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    FoursquareAPIClient mClient;
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mClient = FoursquareAPIClient.getInstance(this);
+        mClient.getLocations();
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.location_list);
     }
 }
